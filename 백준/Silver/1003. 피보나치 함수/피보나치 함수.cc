1d3bb91c7373d1fe1b92ab7e44a874e05b6c98e0
Yun Jae <50546745+YUNJAEGOONER@users.noreply.github.com>
2023-03-05 18:11:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,42 @@
+//
+// Created by YunJae on 2023-03-05.
+//
+
+#include <iostream>
+using namespace std;
+
+long long dp_f[41][2] = {0,};
+
+long long * dp_fibo(int x){
+    
+    if(x == 1){
+        dp_f[1][0] = 0;
+        dp_f[1][1] = 1;
+        return dp_f[1];
+    }
+    if (x == 0){
+        dp_f[0][0] = 1;
+        dp_f[0][1] = 0;
+        return dp_f[0];
+    }
+    else{
+        dp_f[x][0] = dp_f[x-1][0] + dp_f[x-2][0];
+        dp_f[x][1] = dp_f[x-1][1] + dp_f[x-2][1];
+        return dp_f[x];
+    }
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    while(n--){
+        int x;
+        cin >> x;
+        for(int i = 0; i < x ; i ++ ){
+            dp_fibo(i);
+        }
+        cout << dp_fibo(x)[0] << " " << dp_fibo(x)[1] << '\n';
+    }
+
+}
