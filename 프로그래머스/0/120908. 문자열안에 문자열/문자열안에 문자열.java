912eeed673c35efd659de1b47be46dac1d16bb16
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-17 10:31:54 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.09 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,8 +1,18 @@
 class Solution {
     public int solution(String str1, String str2) {
-        if(str1.indexOf(str2) != -1){
-            return 1;
+        // if(str1.indexOf(str2) != -1){
+        //     return 1;
+        // }
+        // return 2;(
+        int answer = 2;
+        for(int i = 0 ; i < str1.length() - str2.length() + 1; i ++){
+            int count = 0;
+            for(int j = 0 ; j < str2.length(); j ++ ){
+                if(str1.charAt(i + j) == str2.charAt(j))count++;
+            }
+            if(count == str2.length())answer=1;
         }
-        return 2;
+        return answer;
     }
+        
 }
