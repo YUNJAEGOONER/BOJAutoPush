428f0f08fc5c2bf3abaec647599473d3b9522349
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-19 11:27:02 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int price){ 
+        int answer = price;
+        
+        if (price >= 500000) answer = (int)(answer * 0.8);
+            //System.out.println(price * 0.8);
+        else if (price >= 300000) answer = (int)(answer * 0.9);
+            //System.out.println(price * 0.9);
+        else if(price >= 100000) answer = (int)(answer * 0.95);
+            //System.out.println(price * 0.95);
+        
+        return answer;
+    }
+}
