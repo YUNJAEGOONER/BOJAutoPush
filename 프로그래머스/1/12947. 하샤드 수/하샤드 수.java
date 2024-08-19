c2d07ae10a142ce23bcfff2271b4db0c98129566
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-19 21:19:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.15 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String str = Integer.toString(x);
+        int sum = 0;
+        for(int i = 0 ; i < str.length() ; i ++){
+            sum = sum + (str.charAt(i) - '0');
+        }
+        if(x % sum != 0) answer = false;
+        
+        return answer;
+    }
+}
