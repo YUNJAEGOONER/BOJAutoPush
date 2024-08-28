be5365e6a3f2ee40f6e904d47f490a0a295a1285
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-28 16:49:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.12 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,9 +1,10 @@
 class Solution {
     public long solution(long n) {
         long answer = -1;
-        if(n % Math.sqrt(n) == 0){
-            answer = (long)((Math.sqrt(n) + 1) * (Math.sqrt(n) + 1));
-        }
+        // if(n % Math.sqrt(n) == 0){
+        //     answer = (long)Math.pow(Math.sqrt(n) + 1 , 2);
+        // }
+        answer = n % Math.sqrt(n) == 0 ? (long)Math.pow(Math.sqrt(n) + 1, 2) : -1;
         return answer;
     }
 }
