c5758176ef09ced6bba0d4038e8ed36e44d72c9a
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-29 10:58:36 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        answer = new int [num_list.length];
+        for(int i = 0; i < num_list.length ; i ++ ){
+            if(i < n)answer[i + (num_list.length - n)] = num_list[i];
+            else answer[i - n] = num_list[i];
+        }
+        return answer;
+    }
+}
