704a207ae17422ef96390d62cd0599c7e87210fa
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-29 18:33:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = false;
         if(s.length() == 4 || s.length() == 6){
-            int count = 0;
+            answer = true;
             for(int i = 0 ; i < s.length() ; i ++ ){
-                if('0' <= s.charAt(i) && s.charAt(i) <= '9'){
-                    count++;
+                if('0' > s.charAt(i) || s.charAt(i) > '9'){
+                    answer = false;
+                    break;
                 }
             }
-            answer = (count == s.length()) ? true : false;
         }
         return answer;
     }
