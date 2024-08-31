a56d3f5100667d618b6502e9dabd5453846cd86f
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-08-31 13:42:40 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,37 +1,13 @@
 class Solution {
-    int [] students;
-    int [] visit;
-    int answer = 0;
-    
-    public void check_sum(){
-        int sum = 0;
-        for(int i = 0 ; i < visit.length; i ++ ){
-            if(visit[i] == 1){
-                sum = sum + students[i];
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0 ; i < number.length - 2; i ++ ){
+            for(int j = i + 1 ; j < number.length - 1; j ++ ){
+                for(int k = j + 1; k < number.length ; k ++ ){
+                    if(number[i] + number[j] + number[k] == 0) answer ++;
+                }
             }
         }
-        if(sum == 0)answer++;
-    }
-    
-    public void dfs_combi(int idx, int cnt){
-        if(cnt == 3){
-            check_sum();
-            return;
-        }
-        for(int i = idx ; i < visit.length; i ++ ){
-            if(visit[i] == 1)continue;
-            visit[i] = 1;
-            dfs_combi(i , cnt + 1);
-            visit[i] = 0;
-        }
-    }
-    
-    
-    public int solution(int[] number) {
-        Solution sol = new Solution();
-        sol.visit = new int[number.length];
-        sol.students = number;
-        sol.dfs_combi(0, 0);
-        return sol.answer;
+        return answer;
     }
 }
