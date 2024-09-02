4ee35038a61de4fc9e319441429333f7ba45eee3
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-09-02 11:27:01 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.71 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,18 +1,23 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = {};
         answer = new int[score.length];
         
-        List<Integer> temp_score = new ArrayList<>();
+        //값이 작은 원소가 우선순위를 갖는다.
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
         for(int i = 0 ; i < score.length ; i ++ ){
-            temp_score.add(score[i]);
-            Collections.sort(temp_score, Collections.reverseOrder());
-            if(i <= k - 1)answer[i] = temp_score.get(i);
-            else answer[i] = temp_score.get(k - 1);
+            pq.add(score[i]); //우선순위 큐에 새로운 원소를 추가
+            
+            //명예의 전당에 들어가지 못하는(k를 넘어가는 경우) 원소 제거
+            if(pq.size() > k){ 
+                pq.poll();  
+            }
+            
+            //명예의 전당의 점수 중 가장 작은 원소 가져오기
+            answer[i] = pq.peek();
         }
     
         return answer;
