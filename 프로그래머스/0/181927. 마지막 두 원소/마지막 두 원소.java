76259434e63c6083219e4a133831134ef584de1b
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-09-03 13:41:26 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        int len = num_list.length;
+        answer = new int [len + 1];
+        for(int i = 0 ; i < len; i ++ ){
+            answer[i] = num_list[i];
+        }
+        int last = (num_list[len - 1] > num_list[len - 2]) ?
+            num_list[len - 1] - num_list[len - 2] : num_list[len - 1] * 2;
+        answer[len] = last;
+        return answer;
+    }
+}
