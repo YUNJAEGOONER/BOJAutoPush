879420c9760ac07fe542baa2900b2e081fb55d3d
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-09-12 09:15:35 +0900

    [level 0] Title: rny_string, Time: 18.67 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        for(int i = 0 ; i < rny_string.length() ; i ++ ){
+            if(rny_string.charAt(i) == 'm') answer = answer + "rn";
+            else answer = answer + rny_string.charAt(i);
+        }
+        return answer;
+    }
+}
