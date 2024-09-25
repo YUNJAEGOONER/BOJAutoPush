d8202248fca6e8043d888e976b4dc7265fd03b78
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2024-09-25 11:24:52 +0900

    [level 1] Title: 실패율, Time: 18.82 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        answer = new int [N];
+        
+        int [] users = new int [N + 1];
+        for(int i = 0; i < stages.length; i ++ ){
+            users[stages[i] - 1]++;
+        }
+        
+        HashMap<Integer, Float> map = new HashMap<>();
+        
+        int sum = 0;
+        for(int i = 0 ; i < users.length - 1 ; i ++ ){
+            if(users[i] == 0){
+                map.put(i + 1, (float)0);
+            }
+            else{
+                map.put(i + 1, (float)users[i] / (float)(stages.length - sum));
+            }
+            sum = sum + users[i];
+        }
+        
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((k1, k2)->map.get(k2).compareTo(map.get(k1)));
+        
+        answer = keySet.stream().mapToInt(x->x).toArray();
+        return answer;
+    }
+}
