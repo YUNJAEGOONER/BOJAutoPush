a0ba1622eb79dde46bc644d8936e6ca811cec674
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-02-07 10:14:06 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n == 1 || n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+int solution(int n) {
+    int answer = 0;
+    while(factorial(answer) <= n){
+       answer ++; 
+    }
+    return answer - 1;
+}
