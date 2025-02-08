f0b95b35699858151ce9433bd442e5f9a508043a
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-02-08 21:24:36 +0900

    [level 0] Title: 세 개의 구분자, Time: 58.08 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    string temp = "";
+    for(int i = 0 ; i < myStr.size() ; i ++ ){
+        if(myStr[i] == 'a' || myStr[i] == 'b' || myStr[i] == 'c'){
+            if(temp.size()) answer.push_back(temp);
+            temp = "";
+        }
+        else{
+            temp = temp + myStr[i];
+        }
+    }
+    if(temp.size()) answer.push_back(temp);
+    if(!answer.size()) answer.push_back("EMPTY");
+    return answer;
+}
