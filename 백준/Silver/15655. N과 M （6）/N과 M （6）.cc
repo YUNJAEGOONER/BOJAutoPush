d18d517b0b73fe1ebcec5288962de790108d42eb
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-02-22 13:13:27 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+//
+// Created by Yunjae on 2025-02-22.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> myvec;
+vector<int> myarr;
+vector<int> visited5;
+int maxdepht;
+
+void dfsmn5(int start, int depth) {
+    if(depth == maxdepht) {
+        for(int i = 0 ; i < myarr.size() ; i ++ ) {
+            cout << myarr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = start ; i < myvec.size() ; i ++ ) {
+        if(!visited5[i]) {
+            visited5[i] = 1;
+            myarr.push_back(myvec[i]);
+            dfsmn5(i + 1, depth + 1);
+            visited5[i] = 0;
+            myarr.pop_back();
+        }
+    }
+}
+
+int main() {
+    int a;
+    cin >> a >> maxdepht;
+    for(int i = 0 ; i < a ; i ++ ) {
+        int n;
+        cin >> n;
+        myvec.push_back(n);
+    }
+
+    sort(myvec.begin(), myvec.end());
+
+    visited5 = vector<int>(a);
+    dfsmn5(0,0);
+
+    return 0;
+}
