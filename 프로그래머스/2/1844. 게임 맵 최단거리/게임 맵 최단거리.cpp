87ee6d8d7e6660c65c4dc62847b68924deb35fe7
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-03-17 21:57:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.15 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,51 +1,47 @@
 #include<vector>
+#include<queue>
 #include<iostream>
 using namespace std;
 
 int solution(vector<vector<int> > maps)
 {
     int answer = 0;
-    int result = -1;
     
-    int n = maps.size();
-    int m = maps[0].size();
+    queue<pair<int, pair<int, int>>> mq;
+    mq.push(make_pair(1, make_pair(0,0)));
     
-
-    vector<pair<pair<int, int>, int>> myqueue;
-    myqueue.push_back(make_pair(make_pair(0, 0), 1));
-    maps[0][0] = 0;
-    
-    while(!myqueue.empty()){
-        int dis = myqueue.begin()->second;
-        int cur_i = myqueue.begin()->first.first;
-        int cur_j = myqueue.begin()->first.second;
-        if(cur_i == n - 1 && cur_j == m - 1){
-            result = dis;
+    while(!mq.empty()){
+        int i = mq.front().second.first;
+        int j = mq.front().second.second;
+        int depth = mq.front().first;
+        mq.pop();
+        
+        if(i == maps.size() - 1 && j == maps[0].size() - 1){
+            answer = depth;
             break;
         }
         
-        myqueue.erase(myqueue.begin());
-        //top
-        if(cur_i - 1 >= 0 && maps[cur_i - 1][cur_j]){
-            myqueue.push_back(make_pair(make_pair(cur_i - 1, cur_j),dis + 1));
-            maps[cur_i - 1][cur_j] = 0;
+        if(i - 1 >= 0 && maps[i - 1][j]){
+            mq.push(make_pair(depth + 1, make_pair(i - 1, j)));
+            maps[i - 1][j] = 0;
         }
-        //down
-        if(cur_i + 1 < n && maps[cur_i + 1][cur_j]){
-            myqueue.push_back(make_pair(make_pair(cur_i + 1, cur_j) ,dis + 1));
-            maps[cur_i + 1][cur_j] = 0;
+        if(i + 1 < maps.size() && maps[i + 1][j]){
+            mq.push(make_pair(depth + 1, make_pair(i + 1, j)));
+            maps[i + 1][j] = 0;
         }
-        //left
-        if(cur_j - 1 >= 0 && maps[cur_i][cur_j - 1]){
-            myqueue.push_back(make_pair(make_pair(cur_i, cur_j - 1), dis + 1));
-            maps[cur_i][cur_j - 1] = 0;
+        if(j - 1 >= 0 && maps[i][j - 1]){
+            mq.push(make_pair(depth + 1, make_pair(i, j - 1)));
+            maps[i][j - 1] = 0;            
         }
-        //right
-        if(cur_j + 1 < m && maps[cur_i][cur_j + 1]){
-            myqueue.push_back(make_pair(make_pair(cur_i, cur_j + 1), dis + 1));
-            maps[cur_i][cur_j + 1] = 0;
+        if(j + 1 < maps[0].size() && maps[i][j + 1]){
+            mq.push(make_pair(depth + 1, make_pair(i, j + 1)));
+            maps[i][j + 1] = 0;           
         }
+    
     }
     
-    return result;
+    if(!answer) answer = -1;
+    
+    
+    return answer;
 }
