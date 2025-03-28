72f9c4aa7ebb0cf77fc634bd62c34b3946022923
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-03-28 10:57:53 +0900

    [Silver I] Title: 문자열 교환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.cc
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.cc
@@ -2,49 +2,30 @@
 // Created by Yunjae on 2025-03-28.
 //
 #include <iostream>
-#include <vector>
-#include<algorithm>
 using namespace std;
 
 int main() {
-    
-    string strin;
-    cin >> strin;
-    
-    string str = strin;
-    sort(str.begin(), str.end());
-    string sa = str;
-
-    sort(str.rbegin(), str.rend());
-    string sb = str;
+    string str;
+    cin >> str;
 
     int answer = str.size();
 
-    for(int i = 0 ; i < strin.size() ; i ++ ) {
-        string temp = "";
+    int bcnt = 0;
+    for(auto a : str) if(a == 'b') bcnt++;
+
+    if(bcnt == 0) answer = 0;
+
+    for(int i = 0 ; i < str.size() ; i ++ ) {
         int cnt = 0;
-
-        for(int j = i ; j < i + strin.size() ; j ++ ) {
-            temp += strin[j % strin.size()];
-        }
-
-        if (temp[0] == 'b') {
-            for(int j = 0 ; j < sb.size() ; j ++ ) {
-                if(temp[j] != sb[j]) {
+        if(str[i] == 'b') {
+            for(int j = i ; j < i + bcnt ; j ++ ) {
+                if(str[j % str.size()] != 'b') {
                     cnt ++;
                 }
             }
+            if(answer > cnt) answer = cnt;
         }
-        else {
-            for(int j = 0 ; j < sa.size() ; j ++ ) {
-                if(temp[j] != sa[j]) {
-                    cnt ++;
-                }
-            }
-        }
-        if(answer > cnt) answer = cnt;
     }
-
-    cout << answer / 2 << '\n';
+    cout << answer << '\n';
     return 0;
 }
