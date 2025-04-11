8c8dec893cde81691bcbff7189e62a9723c24f18
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-04-11 16:27:26 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 8 ms, Memory: 2420 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.cc
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.cc
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <stack>
 using namespace std;
 
 int main() {
-    
     int n;
     cin >> n;
 
@@ -17,46 +17,35 @@ int main() {
         int e, h;
         cin >> e >> h;
         mmap[e] = h;
-        if(h == 0) mmap[e] = -1;
-        if(max < e) max = e;
     }
-
-    vector<int> vec;
-    int prev = 0;
-    for(int i = 1 ; i < max + 1 ; i ++) {
-        if(mmap[i] == 0)vec.push_back(prev);
-        else {
-            if(mmap[i] == -1) {
-                vec.push_back(0);
-                prev = 0;
-            }
-            else {
-                vec.push_back(mmap[i]);
-                prev = mmap[i];
-            }
-        }
-    }
-    vector<int> visited(vec.size());
 
     int answer = 0;
-
-    for(int i = 0 ; i < vec.size() ; i ++ ) {
-        int cur_h = 0;
-        if(visited[i] == 0 && vec[i] > 0) {
-            visited[i] = 1;
-            answer ++;
-            cur_h = vec[i];
-            int end_idx = i;
-            while(vec[end_idx] >= cur_h) {
-                end_idx ++;
-                if(vec[end_idx] == cur_h) {
-                    visited[end_idx] = 1;
+    stack<int> stk;
+    for(auto a : mmap) {
+        if(stk.empty()) {
+            if(a.second != 0) {
+                stk.push(a.second);
+                answer ++;
+            }
+        }
+        else {
+            if(stk.top() < a.second) {
+                stk.push(a.second);
+                answer ++;
+            }
+            else {
+                while(!stk.empty() && stk.top() > a.second) {
+                    stk.pop();
+                }
+                if((stk.empty() || (!stk.empty() && stk.top() != a.second)) && a.second != 0) {
+                    stk.push(a.second);
+                    answer ++;
                 }
             }
         }
     }
-    
+
     cout << answer << ' ';
-    
+
     return 0;
 }
