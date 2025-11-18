efa15f2823bc3feccccd49ec62acacf92ec406ab
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-11-18 15:23:47 +0900

    [D3] Title: 로봇 언어, Time: 90 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D3/24001. 로봇 언어/로봇 언어.java
+++ SWEA/D3/24001. 로봇 언어/로봇 언어.java
@@ -19,37 +19,31 @@ public class Solution {
             
             int max = 0;
             
-            int [][] mmap = new int [input.length() + 1][2];
+            int onlyRight = 0;
+            int onlyLeft = 0;
             
-            mmap[0][0] = 0;
-            mmap[0][1] = 0;
-            
-			for(int i = 1 ; i < input.length() + 1 ; i ++ ) {
-				if(arr[i - 1] == 'L') {
-					mmap[i][0] = mmap[i - 1][0] - 1;
-					mmap[i][1] = mmap[i - 1][1] - 1;
+			for(int i = 0 ; i < input.length() ; i ++ ) {
+				if(arr[i] == 'L') {
+					onlyRight --;
+					onlyLeft --;
 				}
-				else if(arr[i - 1] == 'R') {
-					mmap[i][0] = mmap[i - 1][0] + 1;
-					mmap[i][1] = mmap[i - 1][1] + 1;
+				else if(arr[i] == 'R') {
+					onlyRight ++;
+					onlyLeft ++;
 				}
 				else {
-					if(mmap[i - 1][0] < mmap[i - 1][1]) {
-						mmap[i][0] = mmap[i - 1][0] - 1;
-						mmap[i][1] = mmap[i - 1][1] + 1;
-					}
-					else {
-						mmap[i][0] = mmap[i - 1][1] - 1;
-						mmap[i][1] = mmap[i - 1][0] + 1;
-					}
+					onlyRight ++;
+					onlyLeft --;
 				}
 				
-				if(max < Math.abs(mmap[i][0])) {
-					max = Math.abs(mmap[i][0]);
+				if(Math.abs(onlyLeft) > max) {
+					max = Math.abs(onlyLeft);
 				}
-				if(max < Math.abs(mmap[i][1])) {
-					max = Math.abs(mmap[i][1]);
+				else if(Math.abs(onlyRight) > max) {
+					max = Math.abs(onlyRight);
 				}
+						
+				
 			}
 			
 			System.out.println(max);
@@ -63,3 +57,4 @@ public class Solution {
     }
 
 }
+
