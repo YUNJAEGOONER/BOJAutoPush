681346d5647b731eebb50f25da160a4f972211af
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2026-01-03 14:19:05 +0900

    [D2] Title: 두 개의 숫자열, Time: 76 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -1,58 +1,55 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-	
-	public static void main(String [] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int test = Integer.parseInt(br.readLine());
-	
-		for(int t = 0 ; t < test ; t ++ ) {
-			
-			long answer = Long.MIN_VALUE;
-			
-			String NM [] = br.readLine().split(" ");
-			
-			int N = Integer.parseInt(NM[0]);
-			int M = Integer.parseInt(NM[1]);
-			
-			int [] n = new int [N];
-			String input_n [] = br.readLine().split(" ");
-			for(int i = 0 ; i < N ; i ++ ) {
-				n[i] = Integer.parseInt(input_n[i]);
-			}
-			
-			int [] m = new int [M];
-			String input_m [] = br.readLine().split(" ");
-			for(int i = 0 ; i < M ; i ++ ) {
-				m[i] = Integer.parseInt(input_m[i]);
-			}
-		
-			
-			if(N > M){ // M이 짧은거 
-				for(int k = 0 ; k < (N - M + 1) ; k ++ ) {
-					int sum = 0;
-					for(int i = 0 ; i < M ; i ++ ) {
-						sum += (m[i] * n[k + i]);
-					}
-					if(sum > answer) answer = sum;
-				}
-			}
-			else {
-				for(int k = 0 ; k < (M - N + 1) ; k ++ ) {
-					int sum = 0;
-					for(int i = 0 ; i < N ; i ++ ) {
-						sum += (n[i] * m[k + i]);
-					}
-					if(sum > answer) answer = sum;
-				}
-			}
-			
-			System.out.println("#" + (t + 1) + " " + answer);
-			
-		}
-		
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+
+        for(int t = 0 ; t < test ; t ++ ) {
+            String[] NM = br.readLine().split(" ");
+            int N = Integer.parseInt(NM[0]);
+            int M = Integer.parseInt(NM[1]);
+
+            String[] A = br.readLine().split(" ");
+            String[] B = br.readLine().split(" ");
+            List<Integer> arr1 = new ArrayList<>();
+            List<Integer> arr2 = new ArrayList<>();
+
+            if (N >= M) {
+                for (int i = 0; i < A.length; i++) {
+                    arr1.add(Integer.parseInt(A[i]));
+                }
+                for (int i = 0; i < B.length; i++) {
+                    arr2.add(Integer.parseInt(B[i]));
+                }
+            } else {
+                for (int i = 0; i < A.length; i++) {
+                    arr2.add(Integer.parseInt(A[i]));
+                }
+                for (int i = 0; i < B.length; i++) {
+                    arr1.add(Integer.parseInt(B[i]));
+                }
+            }
+
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < arr1.size() - arr2.size() + 1; i++) {
+                int sum = 0;
+                for (int j = 0; j < arr2.size(); j++) {
+                    sum += (arr1.get(j + i) * arr2.get(j));
+                }
+                if (max < sum) {
+                    max = sum;
+                }
+            }
+
+            System.out.println("#" + (t + 1) + " " + max);
+        }
+    }
 
 }
