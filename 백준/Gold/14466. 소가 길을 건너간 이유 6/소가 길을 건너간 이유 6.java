6f14721a68336346bda7a64541924a4f83a79ec0
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2026-01-15 23:13:42 +0900

    [Gold III] Title: 소가 길을 건너간 이유 6, Time: 248 ms, Memory: 22640 KB -BaekjoonHub

--- 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.java
+++ 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -30,6 +31,10 @@ public class Main {
         int R = Integer.parseInt(NKR[2]);
 
         boolean [][][] roadMap = new boolean[N][N][4]; // 상 우 하 좌
+
+        int [][] mmap = new int [N][N];
+
+        int answer = 0;
 
         //도로 R개
         for(int i = 0 ; i < R ; i ++ ){
@@ -65,67 +70,69 @@ public class Main {
             }
         }
 
-        //k개 만큼
-        //소의 위치
-        List<Pair> cowList = new ArrayList<>();
-
+        //k개 만큼의 소의 위치
         for(int k = 0 ; k < K ; k ++ ){
             String cow [] = br.readLine().split(" ");
             int x = Integer.parseInt(cow[0]) - 1;
             int y = Integer.parseInt(cow[1]) - 1;
-            cowList.add(new Pair(x, y));
+            mmap[x][y] = 1;
         }
 
-        int answer = (cowList.size() * (cowList.size() - 1)) / 2;
 
-        for(int i = 0 ; i < cowList.size() ; i ++ ){
-            Pair start = cowList.get(i);
-            int start_x = start.x;
-            int start_y = start.y;
+        boolean [][] visited = new boolean[N][N];
 
-            for(int j = i + 1 ; j < cowList.size() ; j ++ ){
-                Pair target = cowList.get(j);
-                int target_x = target.x;
-                int target_y = target.y;
+        HashMap<Integer, Integer> cowMap = new HashMap<>();
 
-                boolean [][] visited = new boolean[N][N];
-                Queue<Pair> mq = new LinkedList<>();
-                mq.add(new Pair(start_x, start_y));
-                visited[start_x][start_y] = true;
+        int group = 0;
 
-                while (!mq.isEmpty()){
-                    Pair cur = mq.poll();
-                    int cur_x = cur.x;
-                    int cur_y = cur.y;
-
-                    if(cur_x == target_x && cur_y == target_y){
-                        break;
+        //make region group
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = 0 ; j < N ; j ++ ){
+                int cow = 0;
+                if(!visited[i][j]){
+                    Queue<Pair> mq = new LinkedList<>();
+                    mq.add(new Pair(i, j));
+                    if(mmap[i][j] == 1){
+                        cow ++;
                     }
-
-                    for(int d = 0 ; d < 4 ; d ++ ){
-                        int nx = cur_x + dir_x[d];
-                        int ny = cur_y + dir_y[d];
-                        //범위안에 들어가고
-                        if((0 <= nx && nx < N) && (0 <= ny && ny < N)){
-                            if(!visited[nx][ny] && !roadMap[cur_x][cur_y][d]){ // 길을 건너지 않으면
-                                visited[nx][ny] = true;
-                                mq.add(new Pair(nx, ny));
+                    visited[i][j] = true;
+                    while (!mq.isEmpty()){
+                        Pair cur = mq.poll();
+                        int cur_x = cur.x;
+                        int cur_y = cur.y;
+                        for(int d = 0 ; d < 4 ; d ++ ){
+                            int nx = cur_x + dir_x[d];
+                            int ny = cur_y + dir_y[d];
+                            //범위안에 들어가고
+                            if((0 <= nx && nx < N) && (0 <= ny && ny < N)){
+                                if(!visited[nx][ny] && !roadMap[cur_x][cur_y][d]){ // 길을 건너지 않으면
+                                    visited[nx][ny] = true;
+                                    mq.add(new Pair(nx, ny));
+                                    if(mmap[nx][ny] == 1){
+                                        cow += 1;
+                                    }
+                                }
                             }
                         }
                     }
-
-                }
-
-                if(visited[target_x][target_y]){
-                    answer --;
+                    cowMap.put(group ++, cow);
                 }
             }
         }
 
 
+        List<Integer> cows = new ArrayList<>();
+        for(int c : cowMap.keySet()){
+            cows.add(cowMap.get(c));
+        }
+
+        for(int i = 0 ; i < cows.size() ; i ++ ){
+            for(int j = i + 1 ; j < cows.size() ; j ++ ){
+                answer += cows.get(i) * cows.get(j);
+            }
+        }
+
         System.out.println(answer);
-
-
 
     }
 
